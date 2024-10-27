1bd233421631d0d0d97596896aadcddf3ffc1567
kashmoney2000 <kashyapaddanki@gmail.com>
2024-10-27 18:14:17 -0500

    modify index.js

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,24 @@ import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
-import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import { RoughNotation } from 'react-rough-notation'
-import NewsletterForm from '@/components/NewsletterForm'
 import ViewCounter from '@/components/ViewCounter'
+// import { getPosts } from '@/lib/notion'
+// import { getLatestFivePublished } from '@/lib/notionCms'
+import { getPosts } from '@/lib/notion'
 
 const MAX_DISPLAY = 3
 
 export async function getStaticProps() {
-  const posts = await getAllFilesFrontMatter('blog')
-
+  // await go()
+  // const posts = await getAllFilesFrontMatter('blog/blogs')
+  // const posts = postsMetadata.then((results) => {
+  //   return results
+  // })
+  //------------
+  // let posts = await getLatestFivePublished()
+  let posts = await getPosts(process.env.NOTION_DATABASE_ID, 5)
   return { props: { posts } }
 }
 
@@ -25,11 +32,12 @@ export default function Home({ posts }) {
           <div className="pt-6">
             <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
               Hi, I am{' '}
-              <span className="text-primary-color-500 dark:text-primary-color-dark-500">Parth</span>
+              <span className="text-primary-color-500 dark:text-primary-color-dark-500">Koosh</span>
             </h1>
             <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-300">
-              {`Welcome to ${siteMetadata.description}. I am a Data Engineer who is passionate about Data Science and Automation. In my free time, I like developing `}
-              side projects and learning new technologies.
+              {`Welcome to ${siteMetadata.description}. `} I am a Software Engineer who recently
+              decided to write out whatever b.s. I was thinking of ... I mean, delve into the
+              Humanities.
             </h2>
             <p className="pt-5 text-lg leading-7 text-slate-600 dark:text-slate-300 sm:block md:hidden lg:hidden">
               This is my place for{' '}
@@ -42,9 +50,9 @@ export default function Home({ posts }) {
                 animationDuration={2500}
                 className="text-slate-200"
               >
-                thoughts, reflections & everything&nbsp;
+                everything I choose to share.
               </RoughNotation>
-              in between. Have a good read!
+              &nbsp;Hopefully, you'll find it to be useful!
             </p>
             <p className="hidden pt-10 text-lg leading-7 text-slate-600 dark:text-slate-300 md:block">
               This is my place for{' '}
@@ -57,9 +65,9 @@ export default function Home({ posts }) {
                 animationDuration={2500}
                 className="text-slate-200"
               >
-                thoughts, reflections & everything&nbsp;
+                everything I choose to share.
               </RoughNotation>
-              in between. Have a good read!{' '}
+              &nbsp;Hopefully, you'll find it to be useful!
               <div className="mt-8 text-slate-600 dark:text-slate-400">
                 <span className="text-sm">Press</span>{' '}
                 <span className="rounded-md bg-gray-300 p-1 text-sm text-gray-900 dark:bg-gray-400">
@@ -104,38 +112,38 @@ export default function Home({ posts }) {
           <div className="flex items-center justify-center">
             <div className="grid grid-cols-1 grid-rows-3 gap-8 py-12">
               <div className="my-2 grid items-start gap-8">
-                <div className="group relative">
-                  <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="/projects">
-                    <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
-                      <span className="flex items-center space-x-5">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6 -rotate-6 text-green-600"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
-                          />
-                        </svg>
-                        <span className="pr-6 text-gray-900 dark:text-gray-100">What I built</span>
-                      </span>
-                      <span className="pl-6 text-amber-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
-                        Projects&nbsp;&rarr;
-                      </span>
-                    </span>
-                  </Link>
-                </div>
+                {/*<div className="group relative">*/}
+                {/*  <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>*/}
+                {/*  <Link href="/projects">*/}
+                {/*    <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">*/}
+                {/*      <span className="flex items-center space-x-5">*/}
+                {/*        <svg*/}
+                {/*          xmlns="http://www.w3.org/2000/svg"*/}
+                {/*          className="h-6 w-6 -rotate-6 text-green-600"*/}
+                {/*          fill="none"*/}
+                {/*          viewBox="0 0 24 24"*/}
+                {/*          stroke="currentColor"*/}
+                {/*        >*/}
+                {/*          <path*/}
+                {/*            strokeLinecap="round"*/}
+                {/*            strokeLinejoin="round"*/}
+                {/*            strokeWidth="2"*/}
+                {/*            d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"*/}
+                {/*          />*/}
+                {/*        </svg>*/}
+                {/*        <span className="pr-6 text-gray-900 dark:text-gray-100">What I built</span>*/}
+                {/*      </span>*/}
+                {/*      <span className="pl-6 text-amber-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">*/}
+                {/*        Projects&nbsp;&rarr;*/}
+                {/*      </span>*/}
+                {/*    </span>*/}
+                {/*  </Link>*/}
+                {/*</div>*/}
               </div>
               <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-emerald-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="https://parthdesai.vercel.app/">
+                  <Link href="/about">
                     <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                       <span className="flex items-center space-x-5">
                         <svg
@@ -162,37 +170,37 @@ export default function Home({ posts }) {
                 </div>
               </div>
               <div className="my-2 grid items-start gap-8">
-                <div className="group relative">
-                  <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r  from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="https://parthdesai.vercel.app/resume.pdf">
-                    <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
-                      <span className="flex items-center space-x-5">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6 -rotate-6 text-fuchsia-600"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                          <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                          />
-                        </svg>
-                        <span className="pr-6 text-gray-900 dark:text-gray-100">
-                          Hire me!&nbsp;&nbsp;&nbsp;
-                        </span>
-                      </span>
-                      <span className="pl-6 text-primary-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
-                        Resume&nbsp;&rarr;
-                      </span>
-                    </span>
-                  </Link>
-                </div>
+                {/*<div className="group relative">*/}
+                {/*  <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r  from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>*/}
+                {/*  <Link href="https://parthdesai.vercel.app/resume.pdf">*/}
+                {/*    <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">*/}
+                {/*      <span className="flex items-center space-x-5">*/}
+                {/*        <svg*/}
+                {/*          xmlns="http://www.w3.org/2000/svg"*/}
+                {/*          className="h-6 w-6 -rotate-6 text-fuchsia-600"*/}
+                {/*          fill="none"*/}
+                {/*          viewBox="0 0 24 24"*/}
+                {/*          stroke="currentColor"*/}
+                {/*        >*/}
+                {/*          <path d="M12 14l9-5-9-5-9 5 9 5z" />*/}
+                {/*          <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />*/}
+                {/*          <path*/}
+                {/*            strokeLinecap="round"*/}
+                {/*            strokeLinejoin="round"*/}
+                {/*            strokeWidth={2}*/}
+                {/*            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"*/}
+                {/*          />*/}
+                {/*        </svg>*/}
+                {/*        <span className="pr-6 text-gray-900 dark:text-gray-100">*/}
+                {/*          Hire me!&nbsp;&nbsp;&nbsp;*/}
+                {/*        </span>*/}
+                {/*      </span>*/}
+                {/*      <span className="pl-6 text-primary-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">*/}
+                {/*        Resume&nbsp;&rarr;*/}
+                {/*      </span>*/}
+                {/*    </span>*/}
+                {/*  </Link>*/}
+                {/*</div>*/}
               </div>
             </div>
           </div>
@@ -203,11 +211,16 @@ export default function Home({ posts }) {
         <hr className="border-gray-200 dark:border-gray-700" />
         <ul>
           {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+          {posts.slice(0, MAX_DISPLAY).map((post) => {
+            // const { slug, date, title, summary, Type } = post.properties
+            const slug = post.properties.slug
+            const date = post.properties.date.created_time
+            const title = post.properties.title.title[0].plain_text
+            const summary = ''
+            const Type = post.properties.Type
             return (
               <Link
-                href={`/blog/${slug}`}
+                href={`/blog/${post.id.replace(/-/g, '')}`}
                 key={slug}
                 className="group flex bg-transparent bg-opacity-20 px-2 transition duration-100 hover:scale-105 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800"
               >
@@ -228,7 +241,7 @@ export default function Home({ posts }) {
                           <div>
                             <h2 className="text-2xl font-bold leading-8 tracking-tight">
                               <Link
-                                href={`/blog/${slug}`}
+                                href={`/blog/${post.id.replace(/-/g, '')}`}
                                 className="text-gray-900 transition duration-500 ease-in-out hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500"
                               >
                                 {title}
@@ -236,8 +249,8 @@ export default function Home({ posts }) {
                             </h2>
                           </div>
                           <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
+                            {Type.multi_select.map((type) => (
+                              <Tag key={type.name} text={type.name} />
                             ))}
                           </div>
                           <div className="prose max-w-none pt-5 text-gray-500 dark:text-gray-400">
