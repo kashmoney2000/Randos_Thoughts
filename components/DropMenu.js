fef06e9720368b7d38b4a6b38e8cfebdb55a7ffd
kashmoney2000 <kashyapaddanki@gmail.com>
2024-10-27 16:22:08 -0500

    some

--- components/DropMenu.js
+++ components/DropMenu.js
@@ -250,24 +250,24 @@ export default function DropMenu() {
                 </Link>
               )}
             </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/guestbook">
-                  <a
-                    className={classNames(
-                      active
-                        ? 'bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300'
-                        : 'bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    <div className="flex flex-row">
-                      <ChatBubbleIcon className="mr-4 mt-0.5" /> Guestbook
-                    </div>
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
+            {/*<Menu.Item>*/}
+            {/*  {({ active }) => (*/}
+            {/*    <Link href="/guestbook">*/}
+            {/*      <a*/}
+            {/*        className={classNames(*/}
+            {/*          active*/}
+            {/*            ? 'bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300'*/}
+            {/*            : 'bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700',*/}
+            {/*          'block px-4 py-2 text-sm'*/}
+            {/*        )}*/}
+            {/*      >*/}
+            {/*        <div className="flex flex-row">*/}
+            {/*          <ChatBubbleIcon className="mr-4 mt-0.5" /> Guestbook*/}
+            {/*        </div>*/}
+            {/*      </a>*/}
+            {/*    </Link>*/}
+            {/*  )}*/}
+            {/*</Menu.Item>*/}
             <Menu.Item>
               {({ active }) => (
                 <Link href="/uses">
@@ -394,24 +394,24 @@ export default function DropMenu() {
                 </Link>
               )}
             </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/tweets">
-                  <a
-                    className={classNames(
-                      active
-                        ? 'bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300'
-                        : 'bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    <div className="flex flex-row">
-                      <TwitterLogoIcon className="mr-4 mt-0.5" /> Tweets
-                    </div>
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
+            {/*<Menu.Item>*/}
+            {/*  {({ active }) => (*/}
+            {/*    <Link href="/tweets">*/}
+            {/*      <a*/}
+            {/*        className={classNames(*/}
+            {/*          active*/}
+            {/*            ? 'bg-gray-200 text-gray-700 dark:bg-zinc-700 dark:text-gray-300'*/}
+            {/*            : 'bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700',*/}
+            {/*          'block px-4 py-2 text-sm'*/}
+            {/*        )}*/}
+            {/*      >*/}
+            {/*        <div className="flex flex-row">*/}
+            {/*          <TwitterLogoIcon className="mr-4 mt-0.5" /> Tweets*/}
+            {/*        </div>*/}
+            {/*      </a>*/}
+            {/*    </Link>*/}
+            {/*  )}*/}
+            {/*</Menu.Item>*/}
           </div>
         </Menu.Items>
       </Transition>
