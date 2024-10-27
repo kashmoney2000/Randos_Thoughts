5587ecb9472f463b05a2b44e423d533160d2397c
kashmoney2000 <kashyapaddanki@gmail.com>
2024-10-27 18:55:03 -0500

    change header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,10 +1,12 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/snippets', title: 'Snippets' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/books', title: 'Reading List' },
+  { href: '/workout', title: 'Workout Tracker' },
+  // { href: '/snippets', title: 'Snippets' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/guestbook', title: 'Guestbook' },
+  // { href: '/guestbook', title: 'Guestbook' },
 ]
 
 export default headerNavLinks
